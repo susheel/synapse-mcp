6d8db690ef42c24eb1a3b0c7565029e93a43a3b2
Anh Nguyet Vu <anngvu@gmail.com>
2025-09-19 11:25:48 -0700

    Remove informal test scripts

--- test_synapse.py
+++
@@ -1,8 +0,0 @@
-import synapseclient
-import inspect
-
-syn = synapseclient.Synapse()
-print("Methods available in Synapse client:")
-for method_name in dir(syn):
-    if not method_name.startswith('_'):
-        print(method_name)
